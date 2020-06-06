e2cdb29b9e5d5af16a06681b134fe94bfec5270b
teroron <a1908rk@aiit.ac.jp>
2020-06-06 13:33:45 +0000

    Finish user signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
