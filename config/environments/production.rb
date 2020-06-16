c390b381bb933247c4fc52a58b871d9627211960
teroron <a1908rk@aiit.ac.jp>
2020-06-16 04:29:33 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   host = 'radiant-thicket-60547.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
